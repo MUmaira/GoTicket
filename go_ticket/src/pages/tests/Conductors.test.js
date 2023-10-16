544bc2037b3308976c0b502fdb7d77a206b710b1
IT21258312 <99283261+IT21258312@users.noreply.github.com>
2023-10-16 22:28:21 +0530

    test case added

--- go_ticket/src/pages/tests/Conductors.test.js
+++ go_ticket/src/pages/tests/Conductors.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import ConductorsContainer from './Conductors';
+import ConductorsContainer from '../Conductors';
 
 describe('ConductorsContainer', () => {
   test('renders ConductorsPresentation', () => {
